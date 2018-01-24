d1c33d2afeda033980e5481bf8750cbbc11e87db
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-01-24 18:03:59 +0100

    template for method on generation of collectives from design, to written later

---
+++ Generator/GenerateCollectiveFromDesign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+partial class Generator
+{
+    public static int GenerateCollectiveFromDesign(
+        Collective c,
+        Queue<Designer.CollectiveAssemblyPattern> patterns,
+        Queue<Designer.ControllingCharacterAssemblyPattern> controllingCharacterPatterns,
+        Queue<Designer.MemberCharacterAssemblyPattern> memberCharacterPatterns)
+    {
+        throw new NotImplementedException();
+    }
+}
