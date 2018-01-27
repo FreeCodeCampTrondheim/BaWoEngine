4e54b767cb411e6e58314d8942f1f4c76d0353e2
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-01-27 22:23:15 +0100

    added copy methods for complex entities character and collective

---
+++ Entities/Character/CharacterCopy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+
+
+
+public partial class Character : BaseComplexEntity
+{
+    // copies all data
+    public Character GetCopy()
+    {
+        Character newCharacter = new Character();
+        
+        // copies the content of the current array to a new array
+        Array.Copy(
+            personalizationNumbers, 
+            newCharacter.personalizationNumbers, 
+            personalizationNumbers.Length);
+
+        newCharacter.runSituations = runSituations;
+        newCharacter.runOptions = runOptions;
+        newCharacter.runForecasts = runForecasts;
+
+        // uses copy-constructor of collection to make
+        // a new collection with same contents
+        newCharacter.situations = 
+            new List<CharacterSituation>(situations);
+        newCharacter.options =
+            new List<CharacterOption>(options);
+        newCharacter.forecasts =
+            new List<CharacterForecast>(forecasts);
+
+        newCharacter.stats.boolStats = 
+            new List<int>(stats.boolStats);
+        newCharacter.stats.textStats = 
+            new Dictionary<int, int>(stats.textStats);
+        newCharacter.stats.baseNumberStats =
+            new Dictionary<int, double>(stats.baseNumberStats);
+        newCharacter.stats.modifyingNumberStats =
+            new Dictionary<int, double>(stats.modifyingNumberStats);
+
+        return newCharacter;
+    }
+}
---
+++ Entities/Collective/CollectiveCopy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+
+
+
+public partial class Collective : BaseComplexEntity
+{
+    // copies all data except controlling- and member characters
+    public Collective GetCopy()
+    {
+        Collective newCollective = new Collective();
+
+        // copies the content of the current array to a new array
+        Array.Copy(
+            personalizationNumbers,
+            newCollective.personalizationNumbers,
+            personalizationNumbers.Length);
+
+        newCollective.runSituations = runSituations;
+        newCollective.runOptions = runOptions;
+        newCollective.runForecasts = runForecasts;
+
+        // uses copy-constructor of collection to make
+        // a new collection with same contents
+        newCollective.situations =
+            new List<CollectiveSituation>(situations);
+        newCollective.options =
+            new List<CollectiveOption>(options);
+        newCollective.forecasts =
+            new List<CollectiveForecast>(forecasts);
+
+        newCollective.stats.boolStats =
+            new List<int>(stats.boolStats);
+        newCollective.stats.textStats =
+            new Dictionary<int, int>(stats.textStats);
+        newCollective.stats.baseNumberStats =
+            new Dictionary<int, double>(stats.baseNumberStats);
+        newCollective.stats.modifyingNumberStats =
+            new Dictionary<int, double>(stats.modifyingNumberStats);
+
+        return newCollective;
+    }
+}
